77b54bb4d55900376dabc6e3d2bd8db282828da6
giulicarbonari <50453356+giulicarbonari@users.noreply.github.com>
2021-12-12 22:37:43 -0300

    Fixed duplicated annotations

--- main.py
+++ main.py
@@ -381,6 +381,18 @@ def main():  # Wrapper function
     if (anotaciones == 'yes'):
         raw.annotations.save(path.rsplit('\\', 1)[0]+'\\'+ name + ".txt")
         print('Scoring was completed and the data was saved.')
+
+        #Eliminate annotations duplicated
+        lines_seen = set() # holds lines already seen
+        with open(path.rsplit('\\', 1)[0]+'\\'+ name + ".txt", "r+") as f:
+            d = f.readlines()
+            f.seek(0)
+            for i in d:
+                if i not in lines_seen:
+                    f.write(i)   
+                    lines_seen.add(i)
+            f.truncate()
+
     else:
         print('The data was not saved.')
 
